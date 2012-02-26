177815874bd48158e30f2f01fef20a48480433b4
Matt Mayers <matt@mattmayers.com>
2012-02-26 02:15:09 -0800

    added git install recipe

---
+++ cookbooks/git/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Matt Mayers"
+maintainer_email "matt@mattmayers.com"
+license          "All rights reserved"
+description      "Installs/Configures git"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
